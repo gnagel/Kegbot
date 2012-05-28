7d255b70f2cde6f740ec9b81748bdc4463235a43
Glenn Nagel <glenn@beenverified.com>
2012-05-28 11:02:54 -0400

    Added rails app for mock api requests

---
+++ samples/kegbot_mock/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KegbotMock::Application.config.secret_token = '92321971c5befa0c7613043ec55e1e4d16e649f26c05877002ff3b202675074aef22d2de15ef4b151e87ffc5173c48ede3559d26db998009fc85ff260e2116cb'
